155611c0481925280ba84dd46e197b88f1006df6
juan.saravia <juan.saravia@globant.com>
2020-05-31 23:39:52 -0300

    adding test for split case

--- test/src/week2/ClosestPointsTest.kt
+++ test/src/week2/ClosestPointsTest.kt
@@ -6,22 +6,36 @@ import kotlin.system.measureNanoTime
 class ClosestPointsTest {
 
     @Test
-    fun t1() {
+    fun splitPoints() {
         val p1Expected = Point(1.5, 2.5)
         val p2Expected = Point(2.0, 2.0)
         val (p1, p2) = ClosestPoints.closest(
             arrayOf(
+                Point(10.0, 11.0),
                 Point(5.0, 6.0),
                 Point(8.0, 9.0),
-                Point(1.0, 1.0),
                 p2Expected,
+                p1Expected,
+                Point(1.0, 1.0),
                 Point(20.0, 8.0),
-                p1Expected
+                Point(23.0, 18.0)
             )
         )
 
-        assert(p1 == p1Expected)
-        assert(p2 == p2Expected)
+        assert(p1 == p1Expected || p1 == p2Expected)
+        assert(p2 == p2Expected || p2 == p1Expected)
+    }
+
+    @Test
+    fun bigList(){
+        val pointsList = mutableListOf<Point>()
+        for (i in 0..1_000_000) {
+            pointsList.add(Point(i.toDouble(), i.toDouble()))
+        }
+        val lemmanTime = measureNanoTime {
+            ClosestPoints.closest(pointsList.toTypedArray())
+        }
+        println(lemmanTime)
     }
 
     @Test
